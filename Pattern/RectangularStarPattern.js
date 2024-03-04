a8c5f846461cb25677d23c561e4cdc12b290d5d0
Sachin <sachinsunny303@gmail.com>
2024-03-04 10:24:16 +0000

    Rectangular Star Pattern

--- Pattern/RectangularStarPattern.js
+++ Pattern/RectangularStarPattern.js
@@ -9,3 +9,25 @@ Problem Statement: Given an integer N, print the following pattern.
 *****
 
 */
+
+const pattern = (n) => {
+  for (let i = 0; i <= n; i++) {
+    let line = '';
+    for (let j = 0; j <= n; j++) {
+      line += '*';
+    }
+    console.log(line);
+  }
+};
+
+pattern(5);
+
+const OptimisedPattern = (n) => {
+  for (let i = 0; i <= n; i++) {
+    let line = '';
+    line += '*'.repeat(n + 1);
+    console.log(line);
+  }
+};
+
+OptimisedPattern(7);
