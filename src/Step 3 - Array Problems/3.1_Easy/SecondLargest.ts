0e687157f175a917ae47ea7d05d3b2a55313cecf
Sachin <sachinsunny303@gmail.com>
2024-04-10 19:07:52 +0100

    ADD Array easy problems

---
+++ src/Step 3 - Array Problems/3.1_Easy/SecondLargest.ts
@@ -0,0 +1,56 @@
+/*
+Find Second Smallest and Second Largest Element in an array
+
+Problem Statement: Given an array, find the second smallest and second largest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+
+Example 1:
+Input:
+ [1,2,4,7,7,5]
+Output:
+ Second Smallest : 2
+	Second Largest : 5
+Explanation:
+ The elements are as follows 1,2,3,5,7,7 and hence second largest of these is 5 and second smallest is 2
+
+Example 2:
+Input:
+ [1]
+Output:
+ Second Smallest : -1
+	Second Largest : -1
+Explanation:
+ Since there is only one element in the array, it is the largest and smallest element present in the array. There is no second largest or second smallest element present.
+*/
+
+export {};
+const solution = (arr: number[]) => {
+  if (arr.length <= 1) {
+    console.log('no second largest element');
+    return;
+  }
+
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      largest = arr[i];
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > secondLargest && arr[i] < largest) {
+      secondLargest = arr[i];
+    }
+  }
+
+  if (secondLargest == -Infinity || largest == -Infinity) {
+    console.log('no second largest element');
+    return;
+  }
+  console.log('largest', largest);
+  console.log('secondLargest', secondLargest);
+};
+
+const arr = [1, 1, 1];
+solution(arr);
---
+++ src/Step 3 - Array Problems/3.1_Easy/checkSortedArray.ts
@@ -0,0 +1,41 @@
+/*
+Check if an Array is Sorted
+
+Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
+
+Note: Two consecutive equal values are considered to be sorted.
+
+Example 1:
+Input:
+  N = 5, array[] = {1,2,3,4,5}
+Output
+: True.
+Explanation:
+  The given array is sorted i.e Every element in the array is smaller than or equals to its next values, So the answer is True.
+
+
+Example 2:
+Input:
+  N = 5, array[] = {5,4,6,7,8}
+Output
+: False.
+Explanation:
+  The given array is Not sorted i.e Every element in the array is not smaller than or equal to its next values, So the answer is False.
+
+Here element 5 is not smaller than or equal to its future elements.
+*/
+
+export {};
+const solution = (arr: number[]) => {
+  if (arr.length === 1) return true;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const arr = [4, 6, 7, 8];
+console.log(solution(arr));
