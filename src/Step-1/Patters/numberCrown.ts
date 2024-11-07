5f84b6a74693771f06cf34d3f41cd5152c10db39
Sachin <sachinsunny303@gmail.com>
2024-11-07 07:33:04 +0000

    ADD - Number Crown

--- src/Step-1/Patters/numberCrown.ts
+++ src/Step-1/Patters/numberCrown.ts
@@ -18,20 +18,33 @@ Input Format: N = 6
 Result:   
 1          1
 12        21
-12       321
+123      321
 1234    4321
 12345  54321
 123456654321
 */
 
-export { };
+export {};
+
 const pattern = (N: number) => {
-  for (let i = 0; i < N; i++) {
+  let spaces = 2 * (N - 1);
+
+  for (let i = 1; i <= N; i++) {
     let line = "";
-    for (let j = 0; j < i; j++) {
-      line += i;
+
+    for (let j = 1; j <= i; j++) {
+      line += j;
     }
+    for (let j = 1; j <= spaces; j++) {
+      line += " ";
+    }
+    for (let j = i; j >= 1; j--) {
+      line += j;
+    }
+
+    spaces -= 2;
     console.log(line);
   }
 };
+
 pattern(6);
