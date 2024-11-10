ce8235ed7ff9eb97d894c91f25b866b32bae95cb
Sachin <sachinsunny303@gmail.com>
2024-11-10 12:52:11 +0000

    basic problems

---
+++ src/Step-1/BasicMaths/countDigits.ts
@@ -0,0 +1,28 @@
+//Count digits in a number
+//
+//Problem Statement: Given an integer N, return the number of digits in N.
+//
+//Examples
+//
+//Example 1:
+//Input:N = 12345
+//Output:5
+//Explanation:  The number 12345 has 5 digits.
+//
+//Example 2:
+//Input:N = 7789
+//Output: 4
+//Explanation: The number 7789 has 4 digits.
+
+export { };
+
+const countDigits = (Num: number) => {
+  console.log("Number is: ", Num);
+  console.log("length is: ", Num.toString().length);
+
+  let count = 0;
+
+  for ()
+};
+
+countDigits(12345);
