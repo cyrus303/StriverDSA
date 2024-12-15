e90f5b72b5788a97c09d57037ca054d3ac7044f4
Sachin <sachinsunny303@gmail.com>
2024-12-15 12:57:06 +0000

    Add(udemy sorting algorithm): binary search

--- src/Sandbox/SearchingAlgo/binarySearch.ts
+++ src/Sandbox/SearchingAlgo/binarySearch.ts
@@ -1,1 +1,24 @@
 //Binary Search Algorithm
+
+const binarySearch = (inputArr: number[], target: number) => {
+  let left = 0;
+  let right = inputArr.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    console.log(left, middle, right);
+    if (inputArr[middle] === target) {
+      return middle;
+    } else if (target < inputArr[middle]) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+  }
+  return "not found";
+};
+
+const inputArr = [1, 3, 5, 7, 9, 11, 13, 15]; // Sorted array
+const value = 5; // The value to search for
+
+console.log(binarySearch(inputArr, value));
