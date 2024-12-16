edae8fc56f4a57bf5810ad534e258919716ec183
Sachin <sachinsunny303@gmail.com>
2024-12-16 20:22:48 +0000

    Add(Array Medium): spiral matrix and count sum array

---
+++ src/Step-3(Arrays)/Medium/countSumArrayK.ts
@@ -0,0 +1,62 @@
+// 560. Subarray Sum Equals K
+// Medium
+//
+// Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+//
+// A subarray is a contiguous non-empty sequence of elements within an array.
+//
+// Example 1:
+//
+// Input: nums = [1,1,1], k = 2
+// Output: 2
+// Example 2:
+//
+// Input: nums = [1,2,3], k = 3
+// Output: 2
+
+export {};
+
+const numbers = [1, 1, 1, 2, 3, 4, 5, 2, 1, 7, 8, 9];
+const target = 7;
+
+const bruteForceSolution = (arr: number[], target: number) => {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+    for (let j = i; j < arr.length; j++) {
+      sum = sum + arr[j];
+      if (sum === target) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
+
+// console.log(bruteForceSolution(numbers, target));
+
+const optimisedSolution = (arr: number[], target: number) => {
+  const HASHMAP = new Map();
+
+  HASHMAP.set(0, 1);
+  let preSum = 0;
+  let count = 0;
+
+  for (let num of arr) {
+    preSum += num;
+    if (HASHMAP.has(preSum - target)) {
+      count = count + HASHMAP.get(preSum - target);
+      console.log(count);
+    }
+    HASHMAP.set(preSum, (HASHMAP.get(preSum) || 0) + 1);
+  }
+
+  console.log(HASHMAP);
+  return count;
+};
+
+console.log(optimisedSolution(numbers, target));
+
+// const optimalSolution = (arr: number[]) => {};
+
+// console.log(optimalSolution(arr1));
--- src/Step-3(Arrays)/Medium/spiralMatrix.ts
+++ src/Step-3(Arrays)/Medium/spiralMatrix.ts
@@ -24,3 +24,98 @@
 //
 // Output: 1, 2, 3, 6, 9, 8, 7, 4, 5.
 // Explanation: The output of matrix in spiral form.
+
+export {};
+
+const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
+
+const bruteForceSolution = (matrix: number[][]) => {
+  const ans = [];
+  let n = matrix.length;
+  let m = matrix[0].length;
+
+  let top = 0;
+  let left = 0;
+  let bottom = n - 1;
+  let right = m - 1;
+
+  while (left <= right && top <= bottom) {
+    for (let i = left; i <= right; i++) {
+      ans.push(matrix[top][i]);
+    }
+    top++;
+
+    for (let i = top; i <= bottom; i++) {
+      ans.push(matrix[i][right]);
+    }
+    right--;
+
+    if (top <= bottom) {
+      for (let i = right; i >= left; i--) {
+        ans.push(matrix[bottom][i]);
+      }
+      bottom--;
+    }
+
+    if (left <= right) {
+      for (let i = bottom; i >= top; i--) {
+        ans.push(matrix[i][left]);
+      }
+      left++;
+    }
+  }
+  return ans;
+};
+
+console.log(bruteForceSolution(matrix));
+
+const optimisedSolution = (mat: number[][]) => {
+  const ans = [];
+
+  let n = mat.length;
+  let m = mat[0].length;
+
+  let rowBegin = 0;
+  let rowEnd = n - 1;
+
+  let colBegin = 0;
+  let colEnd = m - 1;
+
+  while (rowBegin <= rowEnd && colBegin <= colEnd) {
+    for (let i = colBegin; i <= colEnd; i++) {
+      ans.push(mat[rowBegin][i]);
+    }
+    rowBegin++;
+
+    for (let i = rowBegin; i <= rowEnd; i++) {
+      ans.push(mat[i][colEnd]);
+    }
+    colEnd--;
+
+    if (colBegin <= colEnd) {
+      for (let i = colEnd; i >= colBegin; i--) {
+        ans.push(mat[rowEnd][i]);
+      }
+      rowEnd--;
+    }
+
+    if (rowBegin <= rowEnd) {
+      for (let i = rowEnd; i >= rowBegin; i--) {
+        ans.push(mat[i][colBegin]);
+      }
+      colBegin++;
+    }
+  }
+  return ans;
+};
+
+console.log(optimisedSolution(matrix));
+
+// const optimalSolution = (arr: number[]) => {};
+
+// console.log(optimalSolution(arr1));
