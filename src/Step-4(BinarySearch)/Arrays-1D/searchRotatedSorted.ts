cd79d33ccf7b59707a0a3ba375fe318c53d0bff5
Sachin <sachinsunny303@gmail.com>
2025-01-02 11:47:25 +0000

    Add(Binary Search) - search in rotated sorted array 1

--- src/Step-4(BinarySearch)/Arrays-1D/searchRotatedSorted.ts
+++ src/Step-4(BinarySearch)/Arrays-1D/searchRotatedSorted.ts
@@ -29,7 +29,7 @@
 export {};
 
 const arr = [4, 5, 6, 7, 0, 1, 2];
-const target = 0;
+const target = 10;
 
 // const bruteForceSolution = (arr: number[]) => {};
 //
@@ -40,13 +40,20 @@ const optimisedSolution = (arr: number[], target: number) => {
   let end = arr.length - 1;
 
   while (start <= end) {
-    let mid = Math.floor((start - end) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      start = mid + 1;
+    const mid = Math.floor(start + (end - start) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[start] <= arr[mid]) {
+      if (arr[start] <= target && target <= arr[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
     } else {
-      end = mid - 1;
+      if (arr[mid] <= target && target <= arr[end]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
     }
   }
   return -1;
