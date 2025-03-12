008af2f39e0eb68a088a5ab815d9f98782300af8
Sachin <sachinsunny303@gmail.com>
2025-03-12 07:38:42 +0000

    Recursion and backtracing: apna college youtube video

---
+++ src/UdemyDSA/recursion/ApnaCollege/permutations.ts
@@ -0,0 +1,26 @@
+export {};
+
+const getPermutations = (arr: number[], i: number, ans: number[][]) => {
+  if (i === arr.length) {
+    ans.push([...arr]);
+    return;
+  }
+
+  for (let idx = i; idx < arr.length; idx++) {
+    [arr[i], arr[idx]] = [arr[idx], arr[i]];
+    getPermutations(arr, i + 1, ans);
+
+    [arr[idx], arr[i]] = [arr[i], arr[idx]];
+  }
+};
+
+function main() {
+  const inputArr = [1, 2, 3];
+  let i = 0;
+  const ans: number[][] = [];
+
+  getPermutations(inputArr, i, ans);
+  console.log(ans);
+}
+
+main();
